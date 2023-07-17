f770e02342e0279944ba0b0fc070e7cbdb069b8a
username <youremail@domain.com>
2023-07-17 14:27:11 -0400

    Add darkmode to full-card comp

--- gelato-parlor/src/components/cards/full-card.js
+++ gelato-parlor/src/components/cards/full-card.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import image from '../../assets/images/Full Card Edited (363x250) 72ppi/Pistachio.png'
-import './full-card.css'
+import React, { useState } from 'react';
+import image from '../../assets/images/Full Card Edited (363x250) 72ppi/Pistachio.png';
+import './full-card.css';
 
 function FullCard() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light');
+  };
+
   return (
-    <div className='full-card'>
-        <div className='full-card__image-container'>
+    <div className={`full-card`}>
+      <div className='full-card__image-container'>
         <img className='full-card__image' src={image} alt='placeholder' />
+      </div>
+      <div className={`full-card__content ${mode}`}>
+        <h3 className={`full-card__title ${mode}`}>Pistachio</h3>
+        <p className={`full-card__description ${mode}`}>
+          Harmonious blend: creamy vanilla, roasted pistachios, and tangy pomegranate
+        </p>
+        <div className='full-card__button' onClick={toggleMode}>
+          Grab
         </div>
-        <div className='full-card__content'>
-            <h3 className='full-card__title'>Pistachio</h3>
-            <p className='full-card__description'>Harmonious blend: creamy vanilla, roasted pistachios, and tangy pomegranate</p>
-            <div className='full-card__button'>Grab</div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FullCard
+export default FullCard;
