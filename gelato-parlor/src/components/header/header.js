2222c83485f4b330f1df49233b8474ef40613550
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-17 14:21:49 -0400

    Button working but not perfect

--- gelato-parlor/src/components/header/header.js
+++ gelato-parlor/src/components/header/header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../assets/images/Gentile Logo/Gentile_logo_Brown.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
-      <div className="header-container">
-        <div className="logo-container">
-          <img src={logo} alt="logo" />
+        <div className="header-container">
+            <div className="logo-container">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="toggle-container">
+                <input type="checkbox" className="checkbox" id="checkbox" />
+                <label htmlFor="checkbox" className="checkbox-label">
+                    <FontAwesomeIcon icon={faMoon} />
+                    <FontAwesomeIcon icon={faSun} />
+                    <span className="ball"></span>
+                </label>
+            </div>
         </div>
-        <div className="toggle-container">
-          <button>
-            Toggle Mode
-          </button>
-        </div>
-      </div>
     );
 };
 
